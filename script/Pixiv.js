0b1e86d1ad9a779b885974a564156dd935d411eb
shiyueX <2396125184@qq.com>
2020-04-13 20:25:12 +0800

    Update Pixiv.js

--- script/Pixiv.js
+++ script/Pixiv.js
@@ -3,3 +3,11 @@ body=JSON.parse(body)
 body['response']['user']['is_premium']=true
 body=JSON.stringify(body)
 $done({body})
+
+/*
+[Script]
+Pixiv.js = type=http-response,pattern=https://oauth.secure.pixiv.net/auth/token,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/shiyueX/test/surge/script/Pixiv.js,script-update-interval=0
+
+[MITM]
+hostname = oauth.secure.pixiv.net
+*/
