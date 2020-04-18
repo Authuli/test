192051ac0338f151cd51d1a015b2a5a6b22f0854
shiyueX <2396125184@qq.com>
2020-04-18 21:18:16 +0800

    Update Q-Search.js

--- script/Q-Search.js
+++ script/Q-Search.js
@@ -1,13 +1,6 @@
 #!name=Q-Search
 #!desc=Safari Search Add-ons by Neurogram
 
-# 注：先进入设置更改 Safari 默认搜索为 DuckDuckGO
-# bd：百度搜索
-# gh：GitHub 搜索
-# gm：Google 图片搜索
-# yd：有道词典搜索
-# 无指令则转 Google 搜索
-
 [URL Rewrite]
 ^https:\/\/duckduckgo.com\/\?q=bd\+([^&]+).+ https://www.baidu.com/s?wd=$1 302
 ^https:\/\/duckduckgo.com\/\?q=db\+([^&]+).+ https://m.douban.com/search/?query=$1 302
